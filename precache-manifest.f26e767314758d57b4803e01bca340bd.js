5092bbac3159a4cc3005412f19276a607b712e8f
zhurankou <s.zhurankou@gmail.com>
2018-12-10 21:02:12 -0800

    Updates

---
+++ precache-manifest.f26e767314758d57b4803e01bca340bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c2a3054bec4bed4ebce",
+    "url": "/side-news/static/css/main.8e5d18bf.chunk.css"
+  },
+  {
+    "revision": "2c2a3054bec4bed4ebce",
+    "url": "/side-news/static/js/main.2c2a3054.chunk.js"
+  },
+  {
+    "revision": "788289526ddcfbcd23ff",
+    "url": "/side-news/static/js/1.78828952.chunk.js"
+  },
+  {
+    "revision": "96df1c2b69b68950c7be",
+    "url": "/side-news/static/js/runtime~main.96df1c2b.js"
+  },
+  {
+    "revision": "0a17e75538e6370caed06f7e7b3f4c58",
+    "url": "/side-news/static/media/Placeholder.0a17e755.svg"
+  },
+  {
+    "revision": "cda20193c56584f735319ceb7cc4f137",
+    "url": "/side-news/static/media/Search.cda20193.svg"
+  },
+  {
+    "revision": "a650d1e99a689f786eb5a2b01a1df4c3",
+    "url": "/side-news/index.html"
+  }
+];
